8bc3f32affe843271ee80c009660bcc87a1a87cf
archer <1246506786@qq.com>
2016-08-05 15:49:26 +0800

    分类和tag标签已经本地入库

--- tool/LoadData.py
+++ tool/LoadData.py
@@ -8,22 +8,31 @@
 from pymongo import MongoClient
 
 # connect to mongodb
-client = MongoClient('mongodb://192.168.100.2:27017/bookshelf')
+client = MongoClient('mongodb://127.0.0.1:27017/bookshelf')
+# client = MongoClient('mongodb://192.168.100.2:27017/bookshelf')
 # client = MongoClient('mongodb://rio:VFZPhT7y@192.168.200.22:27017/bookshelf')
 db = client['bookshelf']
 cc = db['classification']
 
 RAW_DATA = []
-with open('/Users/archer/Downloads/test.csv') as F:
+with open('../data/clc.csv') as F:
     for line in F:
-        RAW_DATA.append(line.strip('\n'))
+        RAW_DATA.append(line.strip('\n\r'))
 
+Tags = []
+with open('../data/tag.csv') as F:
+    for line in F:
+        Tags.append(line.strip('\n'))
+
+
+# print RAW_DATA
 for data in RAW_DATA:
+    print data
     # print data.split(' ')[0], data.split(' ')[1]
     document = {
-        "clc_sort_num": data.split(' ')[0],
-        "description": data.split(' ')[1],
-        "tags": 
+        "clc_sort_num": data.split(',')[0],
+        "description": data.split(',')[1],
+        "tags": Tags
     }
     id = cc.insert_one(document).inserted_id
     print id
