91dd8fb66b229faab81d72e8b458ce9f8244c1ab
archer <1246506786@qq.com>
2016-08-07 13:14:10 +0800

    debug

--- static/demo.js
+++ static/demo.js
@@ -50,25 +50,34 @@ wx.ready(function () {
 
   // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
   document.querySelector('#onMenuShareTimeline').onclick = function () {
-    wx.onMenuShareTimeline({
-      title: '互联网之子',
-      link: 'http://movie.douban.com/subject/25785114/',
-      imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
-      trigger: function (res) {
-        // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-        alert('用户点击分享到朋友圈');
-      },
-      success: function (res) {
-        alert('已分享');
-      },
-      cancel: function (res) {
-        alert('已取消');
-      },
-      fail: function (res) {
-        alert(JSON.stringify(res));
-      }
-    });
-    alert('已注册获取“分享到朋友圈”状态事件');
+    // wx.onMenuShareTimeline({
+    //   title: '互联网之子',
+    //   link: 'http://movie.douban.com/subject/25785114/',
+    //   imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
+    //   trigger: function (res) {
+    //     // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+    //     alert('用户点击分享到朋友圈');
+    //   },
+    //   success: function (res) {
+    //     alert('已分享');
+    //   },
+    //   cancel: function (res) {
+    //     alert('已取消');
+    //   },
+    //   fail: function (res) {
+    //     alert(JSON.stringify(res));
+    //   }
+    // });
+    // alert('已注册获取“分享到朋友圈”状态事件');
+    title: '', // 分享标题
+    link: '', // 分享链接
+    imgUrl: 'https://img1.doubanio.com/lpic/s28849368.jpg', // 分享图标
+    success: function () {
+        // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+        // 用户取消分享后执行的回调函数
+    }
   };
 
   // 2.3 监听“分享到QQ”按钮点击、自定义分享内容及分享结果接口
