bdf8d72ecbb974d42169928fe6b6314ff58b00d9
archer <1246506786@qq.com>
2016-08-09 15:37:57 +0800

    如果为搜索到书籍，使用默认标签云和文案

--- services/BookDetail.go
+++ services/BookDetail.go
@@ -29,28 +29,31 @@ func SearchBookdetail(query string) (err error, rtv models.BookDetail, rtvc mode
       // return
     }
   }
+
   // 是在搜不到，就按照V2来搜
+  // if errb != nil {
+  //   beego.Info("使用默认")
+  //   var rtvs []models.BookDetail
+  //   // criteria = bson.M{"clc_sort_num": bson.M{"$regex": bson.RegEx{".*V2*.", ""}}}
+  //   criteria = bson.M{"clc_sort_num": "V2"}
+  //   // errc := Session.DB(DB).C(BookDetailCollection).Find(criteria).One(&rtv)
+  //   errc := Session.DB(DB).C(BookDetailCollection).Find(criteria).Limit(60).All(&rtvs)
+  //   if errc != nil {
+  //     beego.Info(errc)
+  //     errc = errors.New("Server Internal Error")
+  //     return
+  //   }
+  //
+  //   rtv = rtvs[int(RangeRandomFloat(0, 60))]
+  // }
 
+  // 如果找不到，使用默认的
   if errb != nil {
-    beego.Info("使用默认")
-    var rtvs []models.BookDetail
-    // criteria = bson.M{"clc_sort_num": bson.M{"$regex": bson.RegEx{".*V2*.", ""}}}
-    criteria = bson.M{"clc_sort_num": "V2"}
-    // errc := Session.DB(DB).C(BookDetailCollection).Find(criteria).One(&rtv)
-    errc := Session.DB(DB).C(BookDetailCollection).Find(criteria).Limit(60).All(&rtvs)
-    if errc != nil {
-      beego.Info(errc)
-      errc = errors.New("Server Internal Error")
-      return
-    }
-
-    rtv = rtvs[int(RangeRandomFloat(0, 60))]
+    err, rtvc = GetClcInfo("default")
+  } else {
+    // 在classification里面寻找主文案，标签云
+    err, rtvc = GetClcInfo(rtv.ClcSortNum)
   }
-
-  // 在classification里面寻找主文案，标签云
-  beego.Info(rtv.ClcSortNum)
-  err, rtvc = GetClcInfo(rtv.ClcSortNum)
-  beego.Info(err)
 
   return
 }
--- tool/QiniuUploader.py
+++ tool/QiniuUploader.py
@@ -14,10 +14,10 @@ q = Auth(access_key, secret_key)
 #要上传的空间
 bucket_name = ''
 
-Files = []
-with open('names.csv') as F:
-    for line in F:
-        Files.append(line.strip('\n'))
+Files = ["default.png"]
+# with open('names.csv') as F:
+#     for line in F:
+#         Files.append(line.strip('\n'))
 
 for f in Files:
     print f
